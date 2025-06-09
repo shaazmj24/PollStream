5f24258f77a73720cb3d6bf3a06b70b9927e0d98
smeg24 <meghanishaaz@gmail.com>
2025-06-09 22:21:09 +0500

    yah

--- app.py
+++ app.py
@@ -4,8 +4,18 @@ from flask_socketio import SocketIO
 app = Flask(__name__)
 
 @app.route('/')
-def index():   
-    return render_template('index.html') 
+def home():   
+    return render_template('home.html') 
+
+@app.route('/create') 
+def create(): 
+    return render_template('create.html') 
+
+@app.route('/join') 
+def join(): 
+    return render_template('join.html') 
 
 if __name__ == '__main__':  
     app.run(debug=True) 
+
+
