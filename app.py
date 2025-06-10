327da932604afe01664cafd1817ea4f63d4dac29
smeg24 <meghanishaaz@gmail.com>
2025-06-10 16:34:30 +0500

    ya

--- app.py
+++ app.py
@@ -15,12 +15,24 @@ def generate_code():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))  #k=n returns list of random elements, join combine list into one string. 
                                                                                  #the '' represents something between letter. for e.g '-' then A-1-B-B-3
 
-@app.route('/create', method=['POST']) 
+@app.route('/create', methods=['POST', 'GET']) 
 def create(): 
-    poll_code = generate_code()
+    if request.method == 'POST': 
+        question = request.form.get('ques') 
+        options = [request.form.get(str(i)) for i in range(1, 5)] 
+        options = [op for op in options if op]              #if op returns actual value otherwie none  
+        poll_code = generate_code()
+        while poll_code in polls: 
+            poll_code = generate_code()
+        
+        polls[poll_code] = { 
+            "ques": question, 
+            "options": options, 
+        }  
+        return poll_code
     return render_template('create.html') 
  
-@app.route('/join', method=['GET', 'POST'])
+@app.route('/join', methods=['GET', 'POST'])
 def join(): 
     Valid_code = ["abd", "123", "cc"]
     if request.method == 'POST': 
